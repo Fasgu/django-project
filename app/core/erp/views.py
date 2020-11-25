9cababe10ec17d831c11a4e2d46f7ef724b4d9fe
fasgu95 <aguerra95ugas@gmail.com>
2020-11-24 23:35:57 -0500

    Configuracion de las url

--- app/core/erp/views.py
+++ app/core/erp/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+def myFirstView(request):
+    data = {
+        'name': 'Aurora Soldad'
+    }
+    return JsonResponse(data)
