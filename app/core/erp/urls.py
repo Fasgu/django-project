3f296bdce881c9f81f35bcbc86ffeed854bdcb4f
fasgu95 <aguerra95ugas@gmail.com>
2020-11-29 16:41:57 -0500

    Templates

--- app/core/erp/urls.py
+++ app/core/erp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from app.core.erp.views import myFirstView
+from app.core.erp.views import mySecondView
+
+app_name = 'erp'
 
 urlpatterns = [
-    path('uno/', myFirstView),
-    path('dos/', myFirstView)
+    path('uno/', myFirstView, name='vista1'),
+    path('dos/', mySecondView, name='vista2')
 ]
